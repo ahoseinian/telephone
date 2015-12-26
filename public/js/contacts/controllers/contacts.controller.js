2a25b2a2cf6521277bf86c41e37d47388e843d44
Amir Hoseinian <amir.anyware@gmail.com>
2015-12-26 14:30:26 +0330

    delay for search

--- public/js/contacts/controllers/contacts.controller.js
+++ public/js/contacts/controllers/contacts.controller.js
@@ -1,44 +1,51 @@
-(function(){
-	'use strict';
-	angular
-		.module('app.routes.contacts')
-		.controller('ContactsController', ContactsController);
+(function () {
+  'use strict';
+  angular
+    .module('app.routes.contacts')
+    .controller('ContactsController', ContactsController);
 
-		ContactsController.$inject = ['contact', 'message', '$sce'];
-		function ContactsController(contact, message, $sce){
-			var vm = this;
-			vm.message = message;
-			vm.contacts = contact.contacts;
-			
-			vm.remove = remove;
-			vm.addTel = addTel;
-			vm.removeTel = removeTel;
-			vm.search = search;
-			vm.highlight = highlight;
+  ContactsController.$inject = ['contact', 'message', '$sce', '$timeout'];
 
-			function remove(id){
-				contact.remove(id);
-			}
+  function ContactsController(contact, message, $sce, $timeout) {
+    var vm = this;
+    vm.message = message;
+    vm.contacts = contact.contacts;
 
-			function addTel(ct, tel){
-				contact.addTel(ct, tel);
-			}
+    vm.remove = remove;
+    vm.addTel = addTel;
+    vm.removeTel = removeTel;
+    vm.search = search;
+    vm.highlight = highlight;
 
-			function removeTel(ct, tel){
-				contact.removeTel(ct, tel);	
-			}
+    function remove(id) {
+      contact.remove(id);
+    }
 
-			function search () {
-				if(vm.query.length > 1) contact.search(vm.query);
-			}
+    function addTel(ct, tel) {
+      contact.addTel(ct, tel);
+    }
 
-			function highlight (text, search) {
-				if (!search) {
-	        return $sce.trustAsHtml(text);
-	    	}
-	    	if(text){
-	    		return $sce.trustAsHtml(text.replace(new RegExp(search, 'gi'), '<mark>$&</mark>'));
-	    	}
-			}
-		}
+    function removeTel(ct, tel) {
+      contact.removeTel(ct, tel);
+    }
+
+    function search() {
+      var tempQuery = vm.query;
+      $timeout(function () {
+        if (tempQuery == vm.query) {
+          if (vm.query.length !== 1) contact.search(vm.query);
+        }
+      }, 1000);
+
+    }
+
+    function highlight(text, search) {
+      if (!search) {
+        return $sce.trustAsHtml(text);
+      }
+      if (text) {
+        return $sce.trustAsHtml(text.replace(new RegExp(search, 'gi'), '<mark>$&</mark>'));
+      }
+    }
+  }
 })();

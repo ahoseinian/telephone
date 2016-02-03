3018f098aff07910beb2fdf770920de880fe82f4
Amir Hoseinian <amir.anyware@gmail.com>
2016-02-03 13:55:39 +0330

    return to index after save

--- public/js/contacts/controllers/contacts.new.controller.js
+++ public/js/contacts/controllers/contacts.new.controller.js
@@ -4,14 +4,15 @@
 		.module('app.routes.contacts')
 		.controller('ContactsNewController', ContactsNewController);
 
-		ContactsNewController.$inject = ['contact', '$stateParams'];
-		function ContactsNewController(contact, $stateParams){
+		ContactsNewController.$inject = ['contact', '$stateParams', '$state'];
+		function ContactsNewController(contact, $stateParams, $state){
 			var vm = this;
 			vm.contact = contact.find($stateParams.id);
 			vm.add = add;
 			
 			function add(){
 				vm.contact = contact.save(vm.contact);
+				$state.go('contacts.column');
 			}
 		}
 })();
